d6cc19f0781bdefc627e5c9e29a076e1b199ce21
onjamm <75151498+onjamm@users.noreply.github.com>
2026-06-02 15:48:42 -0700

    finished the live code 2

--- src/Traversal.java
+++ src/Traversal.java
@@ -1,26 +1,65 @@
 public class Traversal {
-  public static void main(String[] args) {
-    TreeNode root = new TreeNode(10);
+    public static void main(String[] args) {
+    TreeNode<Integer> root = new TreeNode<>(10);
 
 
-    root.left = new TreeNode(9);
-    root.left.left = new TreeNode(5);
-    root.left.right = new TreeNode(2);
+    root.left = new TreeNode<>(9);
+    root.left.left = new TreeNode<>(5);
+    root.left.right = new TreeNode<>(2);
 
 
-    root.right = new TreeNode(15);
-    root.right.left = new TreeNode(-3);
-    root.right.right = new TreeNode(5);
-    root.right.right.right = new TreeNode(22);
-
-    // preorder(root);
-    postorder(root);
-    // inorder(root);
-
-  }
+    root.right = new TreeNode<>(15);
+    root.right.left = new TreeNode<>(-3);
+    root.right.right = new TreeNode<>(5);
+    root.right.right.right = new TreeNode<>(22);
 
 
-    public static void preorder(TreeNode current) {
+    TreeNode<String> stringRoot = new TreeNode<>("hello");
+
+
+    stringRoot.left = new TreeNode<>("hi");
+    stringRoot.left.left = new TreeNode<>("sdkjf");
+    stringRoot.left.right = new TreeNode<>("asdf");
+
+
+    stringRoot.right = new TreeNode<>(";klj");
+    stringRoot.right.left = new TreeNode<>("yo");
+    stringRoot.right.right = new TreeNode<>("howdy");
+    stringRoot.right.right.right = new TreeNode<>("santa");
+    // preorder(stringRoot);
+    // // preorder(root); 
+    // // postorder(root);
+    // // inorder(root);
+    // printGreaterThan(root, 1);
+    System.out.println(countNodes(root));
+    }
+
+
+    public static int countNodes(TreeNode<?> current) {
+        if (current == null) {
+            return 0;
+        }
+
+        int leftCount = countNodes(current.left);
+        int rightCount = countNodes(current.right);
+
+        int totalCount = 1 + leftCount + rightCount;
+
+        return totalCount;
+    }
+
+    //Do a preorder traversal 
+    // but ONLY PRINT the NODES that are Greater than our threshold
+    public static void printGreaterThan(TreeNode<Integer> current, int threshold) {
+        if (current == null) return;
+        if (current.data > threshold) {
+            System.out.println(current.data);
+            
+        }
+        printGreaterThan(current.left, threshold);
+        printGreaterThan(current.right, threshold);
+    }
+    public static void preorder(TreeNode<?> current) {
         if(current == null) return;
         
         System.out.println(current.data);
@@ -28,14 +67,18 @@ public class Traversal {
         preorder(current.right);
     }
 
-    public static void postorder(TreeNode current) {
+    //E inforces the types in the list - tighter bounds on type safety
+    //Generics give us much more type safty then doing <Object>, which would
+    //allow any object in the tree, instead of enforcing cohesive types
+    public static <E> void postorder(TreeNode<E> current) {
         if(current == null) return;
         postorder(current.left);
         postorder(current.right);
-        System.out.println(current.data);        
+        E myData = current.data;
+        System.out.println(myData);        
     }
 
-    public static void inorder(TreeNode current) {
+    public static void inorder(TreeNode<?> current) {
         if(current == null) return;
         inorder(current.left);
         System.out.println(current.data);
--- src/TreeNode.java
+++ src/TreeNode.java
@@ -1,21 +1,25 @@
-public class TreeNode {
-    public int data;
-    public TreeNode left;
-    public TreeNode right;
+public class TreeNode <T> {
+    public T data;
+    public TreeNode<T> left;
+    public TreeNode<T> right;
     
-    public TreeNode(int data) {
+    public TreeNode(T data) {
         this.data = data;
     }
 
-    public TreeNode(int data, TreeNode left) {
+    public TreeNode(T data, TreeNode<T> left) {
         this.data = data;
         this.left = left;
         
     }
 
-    public TreeNode(int data, TreeNode left, TreeNode right) {
+    //type safety - will tell us at compile time - which is much better than something failing at runtime
+    //worse for somethign to crash in production, then in development (compilation time))
+    //TreeNode<Integer> myNode = new TreeNode<>(8, null, someOtherNode);
+    public TreeNode(T data, TreeNode<T> left, TreeNode<T> right) {
         this.data = data;
         this.left = left;
-        this.right = left;
+        this.right = right;
     }
+
 }
